65a4f305e1d546858075518326c64fdb637b4377
Vamshi <vamshichiluka13@gmail.com>
2023-09-07 17:59:22 +0530

    updated samples with default export

--- script-runtime-samples/communication-iframe-script-runtime/src/code.js
+++ script-runtime-samples/communication-iframe-script-runtime/src/code.js
@@ -9,8 +9,8 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import { AddOnSdkApi } from "AddOnSdkApi";
-const { runtime } = AddOnSdkApi.instance;
+import AddOnScriptSdk from "AddOnScriptSdk";
+const { runtime } = AddOnScriptSdk.instance;
 
 async function callUIApis() {
     const uiApis = await runtime.apiProxy("panel");
--- script-runtime-samples/editor-apis/src/code.js
+++ script-runtime-samples/editor-apis/src/code.js
@@ -9,10 +9,10 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import { AddOnSdkApi } from "AddOnSdkApi";
+import AddOnScriptSdk from "AddOnScriptSdk";
 import { editor, utils } from "express";
 
-const { runtime } = AddOnSdkApi.instance;
+const { runtime } = AddOnScriptSdk.instance;
 
 async function start() {
     const scriptApi = {
