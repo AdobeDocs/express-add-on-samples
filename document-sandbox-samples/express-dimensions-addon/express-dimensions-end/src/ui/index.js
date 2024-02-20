5090db313357f0ef467f039ba435d6641737535e
Holly Schinsky <hschinsk@adobe.com>
2024-02-20 18:02:46 -0500

    Adds back Davide's dimensions samples

---
+++ document-sandbox-samples/express-dimensions-addon/express-dimensions-end/src/ui/index.js
@@ -0,0 +1,56 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import "@spectrum-web-components/styles/typography.css";
+import "@spectrum-web-components/theme/src/themes.js";
+import "@spectrum-web-components/theme/theme-light.js";
+import "@spectrum-web-components/theme/express/theme-light.js";
+import "@spectrum-web-components/theme/express/scale-medium.js";
+import "@spectrum-web-components/theme/sp-theme.js";
+
+import "@spectrum-web-components/button/sp-button.js";
+
+import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+
+addOnUISdk.ready.then(async () => {
+  console.log("addOnUISdk is ready for use.");
+
+  // Get the Authoring Sandbox.
+  const { runtime } = addOnUISdk.instance;
+  const sandboxProxy = await runtime.apiProxy("documentSandbox");
+
+  const buttonIds = ["logNode", "drawDimensions", "drawDimensionsRefactored"];
+
+  buttonIds.forEach((id) => {
+    const button = document.getElementById(id);
+    button.addEventListener("click", () => {
+      sandboxProxy[id]();
+    });
+    button.disabled = false;
+  });
+
+  runtime.exposeApi({
+    flashWrongElement(id) {
+      const button = document.getElementById(id);
+      const oldText = button.textContent;
+      const oldColor = button.style.backgroundColor;
+
+      button.textContent = "WRONG NODE";
+      button.style.backgroundColor = "#ff0000";
+
+      setTimeout(() => {
+        button.textContent = oldText;
+        button.style.backgroundColor = oldColor;
+      }, 1000);
+    },
+  });
+});
