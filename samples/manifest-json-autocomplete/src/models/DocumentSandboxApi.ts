76499c6eedea957ffcd7d7c9aa28d60439b691a3
Erin F. <erin.finnegan@gmail.com>
2025-09-10 12:56:24 -0400

    Revert "Sample manifest json autocomplete in Vs Code and Cursor"

--- samples/manifest-json-autocomplete/src/models/DocumentSandboxApi.ts
+++
@@ -1,4 +0,0 @@
-// This interface declares all the APIs that the document sandbox runtime ( i.e. code.ts ) exposes to the UI/iframe runtime
-export interface DocumentSandboxApi {
-    createRectangle(): void;
-}
